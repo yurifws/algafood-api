20dcaca5e536855e7c6043598076a7bea72d62f6
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-09 16:58:14 -0300

    Adicionando um objeto no banco de dados, @Transactional

--- src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.jpa;
 
+import java.util.List;
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -7,7 +9,7 @@ import org.springframework.context.ApplicationContext;
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public class ConsultaCozinhaMain {
+public class InclusaoCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -16,18 +18,9 @@ public class ConsultaCozinhaMain {
 
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
-
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
 		
-		cozinha1 = cadastroCozinha.adicionar(cozinha1);
-		cozinha2 = cadastroCozinha.adicionar(cozinha2);
-		
-		System.out.printf("%s - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%s - %s\n", cozinha2.getId(), cozinha2.getNome());
-
+		cozinhas.forEach(c -> System.out.println(c.getNome()));
 	}
 
 }
