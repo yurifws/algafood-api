5886d8794878fa12c6c5f754b34afc9191c7b3cc
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-09 16:42:24 -0300

    Consultando objetos do banco de dados

---
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public List<Cozinha> listar(){
+		return entityManager.createQuery("from Cozinha", Cozinha.class).getResultList();
+	}
+}
