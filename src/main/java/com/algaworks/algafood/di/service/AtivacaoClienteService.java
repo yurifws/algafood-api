64d36eee3a91bdb69036cef37bbd1eb73cccb29a
yurifws <yurifws@gmail.com>
2020-01-08 22:07:21 -0300

    Ajuste versao Java 12 + .gitignore

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,10 +1,5 @@
 package com.algaworks.algafood.di.service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,37 +9,18 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
 
-//@Component
-public class AtivacaoClienteService implements InitializingBean, DisposableBean{
+@Component
+public class AtivacaoClienteService {
 
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
 	
-	//@PostConstruct
-//	public void init() {
-//		System.out.println("INIT "+notificador);
-//	}
-	
-
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
 		this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 
 	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		System.out.println("INIT "+notificador);
-	}
-
-	//@PreDestroy
-	@Override
-	public void destroy() throws Exception{
-		System.out.println("DESTROY AtivacaoClienteService");
-	}
-	
-	
 
 }
