2f1bf7db961b371933965cd8f0d502a2f1664563
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-09 10:52:26 -0300

    configuração do projeto dependendo do ambiente - Spring Profiles

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +11,17 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmailMock implements Notificador {
 	
+	@Autowired
+	private NotificadorProperties properties;
+	
 	public NotificadorEmailMock() {
 		System.out.println("NotificadorEmail Mock");
 	}
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: "+properties.getHostServidor());
+		System.out.println("Porta: "+properties.getPortaServidor());
+		
 		System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
