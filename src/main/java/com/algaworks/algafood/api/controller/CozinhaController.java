c0d73cf6f5b4cd63af5b41a8fee07f20f9316a4a
yurifws <yurifws@gmail.com>
2020-01-12 17:50:42 -0300

    @Controller @ResponseBody to @RestController

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,16 +3,14 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
 	
