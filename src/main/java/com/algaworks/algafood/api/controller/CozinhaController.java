fcefaa39058287a6d6d9581c029cf90a27504e9c
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-13 17:57:51 -0300

    Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,8 +40,12 @@ public class CozinhaController {
 		if(cozinha != null) {
 			return ResponseEntity.ok(cozinha);
 		}
-//		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public void adicionar(@RequestBody Cozinha cozinha) {
+		cozinhaRepository.salvar(cozinha);
 	}
 
 }
