fb533a827c67e889e4f8446a438e32ec5f48faea
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-13 11:13:55 -0300

    Customizando a representação em XML com Wrapper e anotações do Jackson; @JacksonXmlRootElement, @JacksonXmlProperty, @JacksonXmlElementWrapper

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -7,27 +7,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@JsonRootName("gastronomia")
+@JsonRootName("cozinha")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_cozinha")
 public class Cozinha {
 	
-	@JsonIgnore
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
 
