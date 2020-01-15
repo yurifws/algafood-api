a94168eee6d416a79a645ec2a17c27754e66d218
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-15 14:33:05 -0300

    Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +15,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 
 	public List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	public List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 //	public List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 	
