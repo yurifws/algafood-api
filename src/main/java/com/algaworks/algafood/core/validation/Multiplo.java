766a409940f194dbd55c5a7056ddc5cdecb0ea14
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-23 17:42:46 -0300

    Criando constraints de validação customizadas com implementação de ConstraintValidator -  foi criado o @Multiplo

--- src/main/java/com/algaworks/algafood/core/validation/Multiplo.java
+++ src/main/java/com/algaworks/algafood/core/validation/Multiplo.java
@@ -12,21 +12,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
-import javax.validation.OverridesAttribute;
 import javax.validation.Payload;
-import javax.validation.constraints.PositiveOrZero;
 
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RUNTIME)
-@Constraint(validatedBy = { })
-@PositiveOrZero
-public @interface TaxaFrete {
-	
-	@OverridesAttribute(constraint = PositiveOrZero.class, name = "message")
-	String message() default "{TaxaFrete.invalida}";
+@Constraint(validatedBy = { MultiploValidatior.class})
+public @interface Multiplo {
+
+	String message() default "Multiplo inválido";
 
 	Class<?>[] groups() default { };
 
 	Class<? extends Payload>[] payload() default { };
+	
+	int numero();
 
 }
