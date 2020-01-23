efbc6622cfb30a2749ba8b995c3dfb73519969e4
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-23 11:11:38 -0300

    Tratando exception de violação de constraints de validação

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -30,6 +31,17 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	public static final String MSG_ERRO_GENERICA_USUARIO_FINAL = 
 			"Ocorreu um erro interno inesperado no sistema. Tente novamente e se "
 			+ "o problema persistir, entre em contato com o administrador do sistema.";
+	
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
+		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail, detail).build();
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<?> handleUncaught(Exception ex,
@@ -159,15 +171,15 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		if (body == null) {
 			body = Problem.builder()
 					.status(status.value())
+					.timestamp(LocalDateTime.now())
 					.title(status.getReasonPhrase())
-					.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
-					.timestamp(LocalDateTime.now()).build();
+					.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL).build();
 		} else if (body instanceof String) {
 			body = Problem.builder()
 					.status(status.value())
+					.timestamp(LocalDateTime.now())
 					.title((String) body)
-					.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
-					.timestamp(LocalDateTime.now()).build();
+					.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL).build();
 		}
 		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
@@ -175,11 +187,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail, String userMessage) {
 		return Problem.builder()
 				.status(status.value())
+				.timestamp(LocalDateTime.now())
 				.type(problemType.getUri())
 				.title(problemType.getTitle())
 				.detail(detail)
-				.userMessage(userMessage)
-				.timestamp(LocalDateTime.now());
+				.userMessage(userMessage);
 	}
 	
 
