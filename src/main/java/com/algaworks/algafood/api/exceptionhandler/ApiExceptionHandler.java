87555b1aec3264496bb5c591b3d5b04368f733f5
yuri.fernando.silva <yuri.fernando.silva@CPX-I677LRSB3WT.dir.svc.accenture.com>
2020-01-24 11:16:10 -0300

    Ajustando Exception Handler para adicionar mensagens de validação em nível de classe

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -46,11 +47,16 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
 		
 		BindingResult bindingResult = ex.getBindingResult();
-		List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-				.map(fieldError -> {
-					String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
-					return Problem.Field.builder()
-						.name(fieldError.getField())
+		List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream()
+				.map(objectError -> {
+					String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
+					
+					String name = objectError.getObjectName();
+					if(objectError instanceof FieldError) {
+						name = ((FieldError) objectError).getField();
+					}
+					return Problem.Object.builder()
+						.name(name)
 						.userMessage(message)
 						.build();
 				})
@@ -58,7 +64,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		
 		Problem problem = createProblemBuilder(status, problemType, detail, detail)
-						.fields(problemFields)
+						.objects(problemObjects)
 						.build();
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
