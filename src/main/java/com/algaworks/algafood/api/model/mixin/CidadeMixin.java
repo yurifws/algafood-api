39a2bd7e3d7aa1e3eec42c7f53441ad4eabea322
yurifws <yurifws@gmail.com>
2020-02-02 14:01:54 -0300

    Refactory Mixins

--- src/main/java/com/algaworks/algafood/api/model/mixin/CidadeMixin.java
+++ src/main/java/com/algaworks/algafood/api/model/mixin/CidadeMixin.java
@@ -3,7 +3,7 @@ package com.algaworks.algafood.api.model.mixin;
 import com.algaworks.algafood.domain.model.Estado;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-public class CidadeMixin {
+public abstract class CidadeMixin {
 
 	@JsonIgnoreProperties(value = "nome", allowGetters = true)
 	private Estado estado;
--- src/main/java/com/algaworks/algafood/api/model/mixin/CozinhaMixin.java
+++ src/main/java/com/algaworks/algafood/api/model/mixin/CozinhaMixin.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public class CozinhaMixin {
+public abstract class CozinhaMixin {
 
 	@JsonIgnore
 	private List<Restaurante> restaurantes = new ArrayList<>(0);
