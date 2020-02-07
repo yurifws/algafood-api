a79f7d7a8a0366cfdbd7a53bc3d7998753228b00
yurifws <yurifws@gmail.com>
2020-02-06 22:09:37 -0300

    Adicionando e usando o ModelMapper

--- src/main/java/com/algaworks/algafood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/RestauranteInputDisassembler.java
@@ -2,25 +2,20 @@ package com.algaworks.algafood.api.assembler;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.input.RestauranteInput;
-import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Component
 public class RestauranteInputDisassembler {
 	
-
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	public Restaurante toDomainObject(@Valid RestauranteInput restauranteInput) {
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(restauranteInput.getCozinha().getId());
-		
-		Restaurante restaurante = new Restaurante();
-		restaurante.setId(restauranteInput.getId());
-		restaurante.setNome(restauranteInput.getNome());
-		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-		restaurante.setCozinha(cozinha);
-		return restaurante;
+		return modelMapper.map(restauranteInput, Restaurante.class);
 	}
 }
