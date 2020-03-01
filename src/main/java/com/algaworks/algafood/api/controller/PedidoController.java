662c88fab7005252abf3b0066501768b3990aba7
yurifws <yurifws@gmail.com>
2020-02-29 21:17:56 -0300

    Limitando os campos retornados pela API com @JsonFilter do Jackson, utillizando MappingJacksonValue, SimpleFilterProvider e SimpleBeanPropertyFilter.

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,8 @@ import com.algaworks.algafood.api.model.input.PedidoInput;
 import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.service.PedidoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -41,9 +46,27 @@ public class PedidoController {
 	private PedidoInputDisassembler pedidoInputDisassembler;
 	
 	@GetMapping
-	public List<PedidoResumoModel> listar(){
-		return pedidoResumoModelAssembler.toCollectionModel(pedidoService.listar());
+	public MappingJacksonValue listar(@RequestParam(required = false) String campos){
+		List<Pedido> pedidos = pedidoService.listar();
+		List<PedidoResumoModel> pedidoResumoModels = pedidoResumoModelAssembler.toCollectionModel(pedidos);
+		
+		MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidoResumoModels);
+		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+		filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+		
+		if(StringUtils.isNotBlank(campos)) {
+			filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+		}
+		
+		pedidosWrapper.setFilters(filterProvider);
+		
+		return pedidosWrapper;
 	}
+	
+//	@GetMapping
+//	public List<PedidoResumoModel> listar(){
+//		return pedidoResumoModelAssembler.toCollectionModel(pedidoService.listar());
+//	}
 	
 	@GetMapping("/{codigo}")
 	public PedidoModel buscar(@PathVariable String codigo){
