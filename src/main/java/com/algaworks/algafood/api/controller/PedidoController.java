4dacc40d52134782a0863fa0e1dad30e77540827
yurifws <yurifws@gmail.com>
2020-03-02 14:19:41 -0300

    Implementando um conversor de propriedades de ordenação

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -24,10 +24,12 @@ import com.algaworks.algafood.api.assembler.PedidoResumoModelAssembler;
 import com.algaworks.algafood.api.model.PedidoModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.api.model.input.PedidoInput;
+import com.algaworks.algafood.core.data.PageableTranslatator;
 import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.filter.PedidoFilter;
 import com.algaworks.algafood.domain.service.PedidoService;
+import com.google.common.collect.ImmutableMap;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -47,6 +49,7 @@ public class PedidoController {
 	
 	@GetMapping
 	public Page<PedidoResumoModel> pesquisar(PedidoFilter filtro, @PageableDefault(size = 10) Pageable pageable){
+		pageable = traduzirPageable(pageable);
 		Page<Pedido> pedidosPage = pedidoService.pesquisar(filtro, pageable);
 		List<PedidoResumoModel> pedidosResumoModel = pedidoResumoModelAssembler.toCollectionModel(pedidosPage.getContent());
 		return new PageImpl<>(pedidosResumoModel, pageable, pedidosPage.getTotalElements());
@@ -68,6 +71,16 @@ public class PedidoController {
 		
 		return pedidoModelAssembler.toModel(pedidoService.salvar(pedido));
 		
+	}
+	
+	private Pageable traduzirPageable(Pageable pageable) {
+		var mapeamento = ImmutableMap.of(
+				"codigo", "codigo",
+				"restaurante.nome", "restaurante.nome",
+				"nomeCliente", "cliente.nome",
+				"valorTotal", "valorTotal"
+				);
+		return PageableTranslatator.translate(pageable, mapeamento);
 	}
 	
 }
