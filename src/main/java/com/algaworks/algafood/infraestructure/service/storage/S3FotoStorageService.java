b3d2b0c3bce7b420b29201dbf8fd724386888c5c
yurifws <yurifws@gmail.com>
2020-03-11 12:00:59 -0300

    Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/algaworks/algafood/infraestructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infraestructure/service/storage/S3FotoStorageService.java
@@ -9,6 +9,7 @@ import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
@@ -50,7 +51,12 @@ public class S3FotoStorageService implements FotoStorageService {
 
 	@Override
 	public void remover(String nomeArquivo) {
-
+		var caminhoArquivo = getCaminhoArquivo(nomeArquivo);
+		var deleteObjectRequest = new DeleteObjectRequest(
+				storageProperties.getS3().getBucket(), 
+				caminhoArquivo);
+		amazonS3.deleteObject(deleteObjectRequest);
+		
 	}
 
 }
