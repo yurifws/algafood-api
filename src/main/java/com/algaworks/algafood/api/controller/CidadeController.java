91b0f04add5ed1881e432aed470f5888fa9160fe
yurifws <yurifws@gmail.com>
2020-08-29 19:56:57 -0300

    Desacoplando anotações do Swagger dos controladores

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -18,22 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.CidadeInputDisassembler;
 import com.algaworks.algafood.api.assembler.CidadeModelAssembler;
-import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.controller.openapi.CidadeControllerOpenApi;
 import com.algaworks.algafood.api.model.CidadeModel;
 import com.algaworks.algafood.api.model.input.CidadeInput;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.service.CidadeService;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-
-@Api(tags = "Cidades")
 @RestController
 @RequestMapping("/cidades")
-public class CidadeController {
+public class CidadeController implements CidadeControllerOpenApi{
 
 	@Autowired
 	private CidadeService cidadeService;
@@ -44,62 +37,34 @@ public class CidadeController {
 	@Autowired
 	private CidadeInputDisassembler cidadeInputDisassembler;
 
-	@ApiOperation(value = "Listagem as cidades")
 	@GetMapping
 	public List<CidadeModel> listar() {
 		return cidadeModelAssembler.toCollectionModel(cidadeService.listar());
 	}
 
-	@ApiOperation(value = "Buscar uma cidade por Id")
-	@ApiResponses(value = {
-			@ApiResponse(code = 400, message = "Id da cidade inválida", response = Problem.class),
-			@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class),
-	})
 	@GetMapping("/{id}")
-	public CidadeModel buscar(@ApiParam(value = "Id de uma cidade", example = "1") 
-	@PathVariable Long id) {
+	public CidadeModel buscar(@PathVariable Long id) {
 		return cidadeModelAssembler.toModel(cidadeService.buscar(id));
 	}
 
-	@ApiOperation(value = "Cadastra uma cidade por Id")
-	@ApiResponses(value = {
-			@ApiResponse(code = 201, message = "Cidade cadastrada")
-	})
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public CidadeModel adicionar(@ApiParam(name = "corpo", value = "Representação de uma nova cidade")
-			@RequestBody @Valid CidadeInput cidadeInput) {
+	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
 		Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
 		return cidadeModelAssembler.toModel(cidadeService.salvar(cidade));
 	}
 
-	@ApiOperation(value = "Atualiza uma cidade por Id")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Cidade atualizada"),
-			@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class),
-	})
 	@PutMapping("/{id}")
-	public CidadeModel atualizar(
-			@ApiParam(value = "Id de uma cidade", example = "1") 
-			@PathVariable Long id, 
-			@ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados")
-			@RequestBody @Valid CidadeInput cidadeInput) {
+	public CidadeModel atualizar(@PathVariable Long id, @RequestBody @Valid CidadeInput cidadeInput) {
 		Cidade cidadeAtual = cidadeService.buscar(id);
 		cidadeInputDisassembler.copyToDomainObject(cidadeInput, cidadeAtual);
 		return cidadeModelAssembler.toModel(cidadeService.salvar(cidadeAtual));
 		
 	}
 
-	@ApiOperation(value = "Exclui uma cidade por Id")
-	@ApiResponses(value = {
-			@ApiResponse(code = 204, message = "Cidade excluída"),
-			@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class),
-	})
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(
-			@ApiParam(value = "Id de uma cidade", example = "1") 
-			@PathVariable Long id) {
+	public void remover(@PathVariable Long id) {
 		cidadeService.remover(id);
 	}
 
---
+++ src/main/java/com/algaworks/algafood/api/controller/openapi/CidadeControllerOpenApi.java
@@ -0,0 +1,53 @@
+package com.algaworks.algafood.api.controller.openapi;
+
+import java.util.List;
+
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.CidadeModel;
+import com.algaworks.algafood.api.model.input.CidadeInput;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Cidades")
+public interface CidadeControllerOpenApi {
+	
+
+	@ApiOperation(value = "Listagem as cidades")
+	public List<CidadeModel> listar();
+
+	@ApiOperation(value = "Buscar uma cidade por Id")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = "Id da cidade inválida", response = Problem.class),
+			@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class),
+	})
+	public CidadeModel buscar(@ApiParam(value = "Id de uma cidade", example = "1") Long id);
+
+	@ApiOperation(value = "Cadastra uma cidade por Id")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Cidade cadastrada")
+	})
+	public CidadeModel adicionar(@ApiParam(name = "corpo", value = "Representação de uma nova cidade") CidadeInput cidadeInput);
+
+	@ApiOperation(value = "Atualiza uma cidade por Id")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Cidade atualizada"),
+			@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class),
+	})
+	public CidadeModel atualizar(
+			@ApiParam(value = "Id de uma cidade", example = "1") Long id, 
+			@ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados") CidadeInput cidadeInput);
+
+	@ApiOperation(value = "Exclui uma cidade por Id")
+	@ApiResponses(value = {
+			@ApiResponse(code = 204, message = "Cidade excluída"),
+			@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class),
+	})
+	public void remover(
+			@ApiParam(value = "Id de uma cidade", example = "1") Long id);
+
+
+}
