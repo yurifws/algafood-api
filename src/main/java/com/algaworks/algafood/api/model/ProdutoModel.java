02f1c42bcc44144e2ad37d5dfb6d51af1bb7e247
yurifws <yurifws@gmail.com>
2020-08-31 02:22:27 -0300

    Descrevendo documentação de endpoints de produtos

--- src/main/java/com/algaworks/algafood/api/model/ProdutoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/ProdutoModel.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.model;
 
 import java.math.BigDecimal;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +10,19 @@ import lombok.Setter;
 @Setter
 public class ProdutoModel {
 	
+	@ApiModelProperty(example = "1")
 	private Long id;
+	
+	@ApiModelProperty(example = "Espetinho de frango")
 	private String nome;
+	
+	@ApiModelProperty(example = "Acompanha farinha e vinagrete")
 	private String descricao;
+	
+	@ApiModelProperty(example = "13.50")
 	private BigDecimal preco;
+	
+	@ApiModelProperty(example = "true")
 	private boolean ativo;
 	
 }
