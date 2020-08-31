c8ed8467eeb231d9acf985391a650940062b76eb
yurifws <yurifws@gmail.com>
2020-08-31 02:13:16 -0300

    Descrevendo documentação de endpoints de associação de restaurantes com usuários

---
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteResponsavelControllerOpenApi.java
@@ -0,0 +1,43 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.UsuarioModel;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Restaurantes")
+public interface RestauranteResponsavelControllerOpenApi {
+
+	@ApiOperation("Lista os usuários responsáveis associados a restaurante")
+    @ApiResponses({
+        @ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
+    })
+	public List<UsuarioModel> listar(
+			@ApiParam(value = "Id do restaurante", example = "1", required = true) Long restauranteId);
+	
+	@ApiOperation("Associação de restaurante com usuário responsável")
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
+        @ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", 
+            response = Problem.class)
+    })
+	public void associar(
+			@ApiParam(value = "Id do restaurante", example = "1", required = true) Long restauranteId,
+			@ApiParam(value = "Id do usuário", example = "1", required = true) Long responsavelId);
+	
+	@ApiOperation("Desassociação de restaurante com usuário responsável")
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
+        @ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", 
+            response = Problem.class)
+    })
+	public void desassociar(
+			@ApiParam(value = "Id do restaurante", example = "1", required = true) Long restauranteId,
+			@ApiParam(value = "Id do usuário", example = "1", required = true) Long responsavelId);
+}
