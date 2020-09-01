2bf2a2a5bb4d3721ad25b4a49a027f54951bb741
yurifws <yurifws@gmail.com>
2020-09-01 17:21:13 -0300

    Adicionando hypermedia nos recursos de usuários

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteResponsavelController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteResponsavelController.java
@@ -1,8 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,7 +29,7 @@ public class RestauranteResponsavelController implements RestauranteResponsavelC
 	private UsuarioModelAssembler responsavelModelAssembler;
 
 	@GetMapping
-	public List<UsuarioModel> listar(@PathVariable Long restauranteId){
+	public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId){
 		Restaurante restaurante = restauranteService.buscar(restauranteId);
 		return responsavelModelAssembler.toCollectionModel(restaurante.getResponsaveis());
 	}
--- src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteResponsavelControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteResponsavelControllerOpenApi.java
@@ -1,6 +1,6 @@
 package com.algaworks.algafood.api.openapi.controller;
 
-import java.util.List;
+import org.springframework.hateoas.CollectionModel;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.UsuarioModel;
@@ -18,7 +18,7 @@ public interface RestauranteResponsavelControllerOpenApi {
     @ApiResponses({
         @ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
     })
-	public List<UsuarioModel> listar(
+	public CollectionModel<UsuarioModel> listar(
 			@ApiParam(value = "Id do restaurante", example = "1", required = true) Long restauranteId);
 	
 	@ApiOperation("Associação de restaurante com usuário responsável")
