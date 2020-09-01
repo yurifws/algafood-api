53cc84f8d033844141e577e909bff1729665c606
yurifws <yurifws@gmail.com>
2020-09-01 17:32:37 -0300

    Corrigindo link de coleção de recurso de responsáveis por restaurante

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteResponsavelController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteResponsavelController.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.api.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
@@ -31,7 +34,10 @@ public class RestauranteResponsavelController implements RestauranteResponsavelC
 	@GetMapping
 	public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId){
 		Restaurante restaurante = restauranteService.buscar(restauranteId);
-		return responsavelModelAssembler.toCollectionModel(restaurante.getResponsaveis());
+		return responsavelModelAssembler.toCollectionModel(restaurante.getResponsaveis())
+				.removeLinks()
+				.add(linkTo(methodOn(RestauranteResponsavelController.class)
+						.listar(restauranteId)).withSelfRel());
 	}
 
 	@PutMapping("/{responsavelId}")
