414b25c75bf3c6adf47513c05834052da1fafe9d
yurifws <yurifws@gmail.com>
2020-09-10 14:32:33 -0300

    Corrigindo a documentação dos endpoints de cidades

--- src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Links;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,8 +23,10 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.CidadeModel;
 import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
+import com.algaworks.algafood.api.openapi.model.CidadesModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.CozinhasModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.LinksModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PageableModelOpenApi;
@@ -72,10 +75,12 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
 				.directModelSubstitute(Links.class, LinksModelOpenApi.class)
 				.alternateTypeRules(
-						AlternateTypeRules.newRule(
-								typeResolver.resolve(Page.class, CozinhaModel.class), CozinhasModelOpenApi.class),
-						AlternateTypeRules.newRule(
-								typeResolver.resolve(Page.class, PedidoResumoModel.class), PedidoResumoModelOpenApi.class))
+						AlternateTypeRules.newRule(typeResolver.resolve(Page.class, CozinhaModel.class), 
+								CozinhasModelOpenApi.class),
+						AlternateTypeRules.newRule(typeResolver.resolve(Page.class, PedidoResumoModel.class), 
+								PedidoResumoModelOpenApi.class),
+						AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, CidadeModel.class), 
+								CidadesModelOpenApi.class))
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"),
 						new Tag("Grupos", "Gerencia as grupos"),
