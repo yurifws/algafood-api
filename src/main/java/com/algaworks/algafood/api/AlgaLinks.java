15f37abfc01cb1be87946bf2f5402e421091e04b
yurifws <yurifws@gmail.com>
2020-09-10 12:05:13 -0300

    Implementando endpoint com links de recursos de estatísticas

--- src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.api.controller.CidadeController;
 import com.algaworks.algafood.api.controller.CozinhaController;
 import com.algaworks.algafood.api.controller.EstadoController;
+import com.algaworks.algafood.api.controller.EstatisticasController;
 import com.algaworks.algafood.api.controller.FormaPagamentoController;
 import com.algaworks.algafood.api.controller.GrupoController;
 import com.algaworks.algafood.api.controller.GrupoPermissaoController;
@@ -310,5 +311,22 @@ public class AlgaLinks {
 	    return linkTo(methodOn(GrupoPermissaoController.class)
 	            .desassociar(grupoId, permissaoId)).withRel(rel);
 	}
+	
+	public Link linkToEstatisticas(String rel) {
+	    return linkTo(EstatisticasController.class).withRel(rel);
+	}
+
+	public Link linkToEstatisticasVendasDiarias(String rel) {
+	    TemplateVariables filtroVariables = new TemplateVariables(
+	            new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("timeOffset", VariableType.REQUEST_PARAM));
+	    
+	    String pedidosUrl = linkTo(methodOn(EstatisticasController.class)
+	            .consultarVendasDiarias(null, null)).toUri().toString();
+	    
+	    return new Link(UriTemplate.of(pedidosUrl, filtroVariables), rel);
+	}   
 	
 }
--- src/main/java/com/algaworks/algafood/api/openapi/controller/EstatisticasControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/EstatisticasControllerOpenApi.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
+import com.algaworks.algafood.api.controller.EstatisticasController.EstatisticasModel;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 
@@ -31,5 +32,8 @@ public interface EstatisticasControllerOpenApi {
 			String timeOffset);
 	
 	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,String timeOffset);
+	
+	@ApiOperation(value = "Estatísticas", hidden = true)
+	EstatisticasModel estatisticas();
 
 }
