778162cf5f8d04b7bce2b91f263508ccb75e45e1
yurifws <yurifws@gmail.com>
2020-10-20 18:31:06 -0300

    Customizando a página de OAuth2 Approval

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/SecurityController.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/SecurityController.java
@@ -10,5 +10,12 @@ public class SecurityController {
 	public String login() {
 		return "pages/login";
 	}
+	
+	@GetMapping("/oauth/confirm_access")
+	public String approval() {
+		return "pages/approval";
+	}
+	
+	
 
 }
